4ac9379beddb39c9a381075cd76f35b45bf0a4cc
fengcaifan <2998000457@qq.com>
2019-07-21 16:12:22 +0800

    准备接入realm

--- Diary/Diary/Base/View/杂记/编辑页面/StoryEditVC.swift
+++ Diary/Diary/Base/View/杂记/编辑页面/StoryEditVC.swift
@@ -11,15 +11,33 @@ import UIKit
 class StoryEditVC: FCFBaseViewController {
     @IBOutlet weak var titleTF: UITextField!
     @IBOutlet weak var desTF: UITextView!
+    @IBOutlet weak var screenImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUI()
         //
+    }
+    
+    func initUI() {
+        let btn = UIButton(type: .custom)
+        btn.setTitle("保存", for: .normal)
+        btn.addTarget(self, action: #selector(saveBtnAction), for: .touchUpInside)
+        let baritem = UIBarButtonItem(customView: btn)
+        self.navigationItem.rightBarButtonItem = baritem
+    }
+    
+    @objc func saveBtnAction() {
+        self.view.endEditing(true)
         
     }
     
+    
     @IBAction func imgTapAction(_ sender: Any) {
-        
+        let x = arc4random() % 26;
+        let imgName = String(format: "%03d", x)
+        print(imgName)
+        screenImg.image = UIImage(named: imgName)
     }
 }
 
